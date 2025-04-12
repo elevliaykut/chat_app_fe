90199ec34dbe6444cef4db4ee8cf6487f9c8b938
Aykut Elevli <elevli.aykut.28@gmail.com>
2025-04-13 01:22:51 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
-const withTM = require('next-transpile-modules')(['antd', 'rc-util']);
+const withTM = require('next-transpile-modules')(['antd', 'rc-util']); // Ant Design ve rc-util modüllerini transpile et
 
 module.exports = withTM({
   reactStrictMode: true,
+  webpack(config, { isServer }) {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+    });
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        module: false,
+        path: false,
+      };
+    }
+    return config;
+  },
 });
