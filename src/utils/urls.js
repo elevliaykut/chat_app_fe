ab7199ed873fcb19a42ac2ef93fb7cc73bcb9df3
Aykut Elevli <elevli.aykut.28@gmail.com>
2025-05-28 00:15:40 +0300

    fix

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,3 +1,4 @@
 //export const BASE_URL = 'https://laravel-app-production-4473.up.railway.app/api';
-export const BASE_URL = 'http://127.0.0.1:8000/api';
+//export const BASE_URL = 'http://127.0.0.1:8000/api';
+export const BASE_URL = 'https://8ada-18-158-101-141.ngrok-free.app/api';
 
