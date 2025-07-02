acd15cafc28bc5d3218a327edecb08304f6d546a
Aykut Elevli <elevli.aykut.28@gmail.com>
2025-07-02 16:00:45 +0300

    fix

--- src/components/molecules/MemberPhotoCard/index.js
+++ src/components/molecules/MemberPhotoCard/index.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import styles from './Index.module.css';
+import { usePathname } from 'next/navigation';
+import ThemeConfig from '@/src/utils/ThemeConfig';
 
 const MemberPhotoCard = ({ 
     photos = [] 
 }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const pathname = usePathname();
+  const isMyProfile = pathname === '/my-profile';
 
   const handleThumbnailClick = (index) => {
     setCurrentIndex(index);
@@ -25,13 +29,29 @@ const MemberPhotoCard = ({
 
         <div className={styles.thumbnailContainer}>
           {photos.map((photo, index) => (
-            <img
-              key={photo.id}
-              src={photo.photo_path}
-              alt={`Thumb ${index + 1}`}
-              className={`${styles.thumbnail} ${index === currentIndex ? styles.activeThumbnail : ''}`}
-              onClick={() => handleThumbnailClick(index)}
-            />
+            <>
+              <div style={{ display: 'block'}}>
+                <img
+                  key={photo.id}
+                  src={photo.photo_path}
+                  alt={`Thumb ${index + 1}`}
+                  className={`${styles.thumbnail} ${index === currentIndex ? styles.activeThumbnail : ''}`}
+                  onClick={() => handleThumbnailClick(index)}
+                />
+                {isMyProfile && (
+                  <>
+                    <label style={{
+                        color: photo?.status === 1 ? ThemeConfig.success : ThemeConfig.error,
+                        fontSize: '11px',
+                        fontWeight: 'bold'
+                      }}>
+                        {photo?.status === 1 ? 'Onaylandı' : 'Onay Bekliyor'}
+                    </label>
+                  </>
+                )}
+              </div>
+            </>
+            
           ))}
         </div>
       </div>
