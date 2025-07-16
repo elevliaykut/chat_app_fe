19ff3c9eea03ca883d2e76ba8968bbc6c696102d
Aykut Elevli <elevli.aykut.28@gmail.com>
2025-07-16 09:53:36 +0300

    fix

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,3 +1,3 @@
 //export const BASE_URL = 'https://laravel-app-production-4473.up.railway.app/api';
-export const BASE_URL = 'http://127.0.0.1:8000/api';
+export const BASE_URL = 'https://www.relaxed-engelbart.45-136-6-30.plesk.page/api';
 
